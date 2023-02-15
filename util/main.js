6460bd286a2fc5b790d2abc386dfd0105134e97c
hanahana <enigma597@mail.ru>
2023-02-15 23:40:56 +0300

    feat: find the way to set wallpaper on windows

---
+++ util/main.js
@@ -0,0 +1,4 @@
+const { execSync } = require('node:child_process');
+
+execSync('reg add "HKEY_CURRENT_USER\\Control Panel\\Desktop" /v Wallpaper /t REG_SZ /d  imagePath', {input: 'Y'})
+execSync('RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters');
