3036ca54dbdc703dceb86ce9d04a6b2f73754396
hanahana <enigma597@mail.ru>
2023-06-04 20:17:49 +0300

    fix(client, server): packing fix

--- code/client/src/electron.prod.js
+++ code/client/src/electron.prod.js
@@ -21,7 +21,7 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, './index.html'),
+    pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }));
